f7baaea5580fe6ba36e9a713e9c44e5faff9be17
jaros072 <jaros072@d.umn.edu>
2015-04-08 16:49:50 -0500

    sa

--- Proj1/src/pkg1/GitClass.java
+++ Proj1/src/pkg1/GitClass.java
@@ -4,6 +4,13 @@ public class GitClass {
 	  public static void main(String[] args) {
 	    System.out.println("Adam Blows");
 	    System.out.println("Adam Blows");
+	    System.out.println("Adam Blows");
+	    System.out.println("Adam Blows");
+	    System.out.println("Adam Blows");
+	    System.out.println("Adam Blows");
+	    
+
+	   
 	  }
 } 
 
